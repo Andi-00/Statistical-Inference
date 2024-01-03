a82621b30dda87712f0982ff43e7b90e63017a31
andi <andreasweber2001@outlook.de>
2024-01-03 14:54:00 +0100

    finished warm up

--- thermal_distribution.py
+++ thermal_distribution.py
@@ -1,6 +1,27 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+plt.rcParams['pgf.rcfonts'] = False
+plt.rcParams['font.serif'] = []
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['text.usetex'] = True
+plt.rcParams['axes.formatter.useoffset'] = False
+plt.rcParams['lines.linewidth'] = 2
+plt.rcParams['errorbar.capsize'] = 2
+plt.rcParams['grid.linewidth'] = 0.5
+plt.rcParams['axes.labelsize'] = 22
+plt.rcParams['axes.titlesize'] = 22
+plt.rcParams['xtick.labelsize'] = 18
+plt.rcParams['ytick.labelsize'] = 18
+plt.rcParams['legend.title_fontsize'] = 20
+plt.rcParams['legend.fontsize'] = 20
+plt.rcParams['savefig.dpi'] = 300
+plt.rcParams['savefig.bbox'] = 'tight'
+plt.rcParams['savefig.pad_inches'] = 0.1
+
+#plt.rcParams['savefig.transparent'] = True
+plt.rcParams['figure.figsize'] = (10, 7)
+
 # Set a random seed
 np.random.seed(3)
 
@@ -34,43 +55,42 @@ def spin_flip(N, s, h, j):
     # Spins to flip
     k = np.random.randint(0, n, N)
 
-    states = []
-    copy = s.copy()
+    # List that stores the different spin configurations
+    states = [s.copy()]
 
     # Loop to flip the spins
     for l in k:
-        # Values that are stored in an array
-        states.append(copy)
-
-        copy = states[-1].copy()
 
         # Computation of the energy difference
-        diff = dH(l, copy, h, j)
+        diff = dH(l, s, h, j)
 
-        # Condition for a spin flip, when its energetically favourable
-        if diff <= 0 : copy[l] *= -1
-        # Otherwise, thermal fluctuations can induce a spin flip with a certain probability
-        elif np.random.rand() < np.exp(-diff) : copy[l] *= -1
+        # Conditions for a spin flip
+        if diff <= 0 : s[l] *= -1
+        elif np.random.rand() < np.exp(-diff) : s[l] *= -1
+
+        # A copy of the current spin configuration is saved in the states
+        states.append(s.copy())
         
     return states
     
 
 # Number of spin flips
-N = int(1E4)
+N = int(2E4)
 
-# Every 10th configuration of the last n_train values is taken as train data
-n_train = int(1E3)
+# Every n_sep-th configuration of the last n_train values is taken as train data
+n_sep = 10
+n_train = int(1E4)
 
 # Perform the spin flips on the spins s
 s = spin_flip(N, s, h, j)
 
 # Only the values in the thermal equilibrium are used to compute the averages
-s = s[- n_train :: 10]
+s = s[- n_train :: n_sep]
 
 # Computation of the correlations
 s2 = np.einsum("ab, ac -> abc", s, s)
 
-# Magnetisation and Correlation
+# Mean Magnetisation and Correlation of the train data
 mean_s = np.mean(s, axis = 0)
 mean_s2 = np.mean(s2, axis = 0)
 
@@ -88,13 +108,19 @@ for i in range(n):
     j_0[:, i] = j[i, :]
 
 # Number of steps for the gradient descend
-m = int(50)
+m = int(370)
 
 # Learning rate a
-a = 0.2
+a = 0.3
 
 # Storage for the loss values
 loss = []
+
+lsq = []
+
+# Stored values of the weights
+# h_history = []
+# j_history = []
 
 # Loop for the gradient descend
 for i in range(m):
@@ -105,7 +131,7 @@ for i in range(m):
 
     # MCMC algorithm for the spin flips of s_0
     s_0 = spin_flip(N, s_0, h_0, j_0)
-    s_0 = s_0[- n_train :: 10]
+    s_0 = s_0[- n_train :: n_sep]
 
     # Computation of the correlations
     s2_0 = np.einsum("ab, ac -> abc", s_0, s_0)
@@ -117,9 +143,42 @@ for i in range(m):
     h_0 += a * (mean_s - mean_s_0)
     j_0 += a * (mean_s2 - mean_s2_0)
 
-    l = np.einsum("b, ab -> a", h_0, s) + np.einsum("ab, bc, ac", s, j_0, s) / 2
+    lsq.append(np.mean(np.append(((h_0 - h) ** 2).flatten(), (((j_0 - j) * n) ** 2).flatten())))
+ 
+    l = np.einsum("b, ab -> a", h_0, s) + np.einsum("ab, bc, ac -> a", s, j_0, s) / 2
     l = -np.mean(l)
     loss.append(l)
+
+    if i % 10 == 0 : 
+        print(i)
+        print(lsq[-1])
+        
+
+fig, ax = plt.subplots()
+
+x = np.arange(len(loss))
+
+ax.plot(x, loss, color = "black")
+ax.scatter(x, loss, color = "crimson", zorder = 10)
+
+ax.grid()
+ax.set_xlabel("Number of steps")
+ax.set_ylabel("Negative log-likelihood $\mathcal L$")
+ax.set_title("Loss during the training")
+
+plt.savefig("./thermal_images/loss_plot.png")
+
+dh = h_0 - h
+dj = (j_0 - j) * n
+
+temp = []
+
+for i in range(n):
+    for k in range(i):
+        temp.append(dj[i, k])
+
+dj = np.array(temp)
+
 
 print(h)
 print(h_0)
@@ -127,7 +186,18 @@ print(j)
 print(j_0)
 
 fig, ax = plt.subplots()
-ax.scatter(np.arange(len(loss)), loss)
+
+errors = np.append(dh.flatten(), dj.flatten())
+
+b = np.arange(-1, 1.12, 0.1) - 0.05
+
+ax.grid()
+ax.hist(errors, bins = b, color = "crimson", edgecolor = "black", zorder  = 10)
+
+ax.set_xlabel(r"Normalised Error $\Delta \theta / \sigma$")
+ax.set_ylabel("Number of counts")
+ax.set_title("Histogram of the Deviations")
+
+plt.savefig("./thermal_images/hisogram.png")
 
 plt.show()
-
