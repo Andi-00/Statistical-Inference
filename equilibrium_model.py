269037284e886b29016ffa551ae1fa3b79b22ff1
andi <andreasweber2001@outlook.de>
2024-01-10 21:30:46 +0100

    comutation of the magnetisation and correlation

--- equilibrium_model.py
+++ equilibrium_model.py
@@ -1,15 +1,22 @@
 import numpy as np
 
-# Creating the mean of the spins and correlations to save them as a txt file
-# This will reduce the time it takes to load the data
-s = np.genfromtxt("./bint.txt", delimiter = " ")
+# # Creating the mean of the spins and correlations to save them as a txt file
+# # This will reduce the time it takes to load the data
+# s = np.genfromtxt("./bint.txt", delimiter = " ")
 
-s[s == 0] = -1
-s2 = np.einsum("ac, bc -> abc", s, s)
+# s[s == 0] = -1
 
-mean_s = np.mean(s, axis = 1)
-mean_s2 = np.mean(s2, axis = 2)
+# s2 = np.zeros((160, 160))
 
-np.savetxt("./mean_spin.txt", s, delimiter = " ")
-np.savetxt("./mean_spin_correlation.txt", s2, delimiter = " ")
+# for i in range(160):
+#     print(i)
+#     for j in range(i + 1):
+#         s2[i, j] = np.mean(s[i, :] * s[j, :])
+#         s2[j, i] = np.mean(s[i, :] * s[j, :])
+
+# mean_s = np.mean(s, axis = -1)
+
+# np.savetxt("./mean_spin.txt", mean_s, delimiter = " ")
+# np.savetxt("./mean_spin_correlation.txt", s2, delimiter = " ")
+
 
